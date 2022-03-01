61dbb7ae5bf48c411fa7ea7b00b0357c5238a762
Ryze Danica <rdanica@si-a1.kzn.21-school.ru>
2022-03-01 17:47:27 +0300

    1

---
+++ module_03/ex02/ClapTrap.hpp
@@ -0,0 +1,28 @@
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+# include <string>
+
+class ClapTrap
+{
+	protected:
+		std::string		name;
+		unsigned int	health;
+		unsigned int	energyPoints;
+		unsigned int	attackDamage;
+
+	public:
+		ClapTrap();
+		~ClapTrap();
+		ClapTrap(std::string name);
+		ClapTrap(const ClapTrap &clap_trap);
+		void	setAttackDamage(unsigned int const ammount);
+		int		getAttackDamage(void) const;
+		void	attack(const std::string& target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
+		ClapTrap &operator = (ClapTrap const &clap_trap);
+};
+
+#endif
